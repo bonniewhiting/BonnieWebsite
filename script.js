6137a824c17541591f0d180d3c65b20dafeed29a
Mason <masonlynass@gmail.com>
2023-09-18 14:31:24 -0700

    some final minor changes

--- script.js
+++ script.js
@@ -29,18 +29,3 @@ button.addEventListener('click', function(){
 //     }
 //   }, 10000)
 // }
-
-const slidesContainer = document.getElementById("press");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slArrowPrev");
-const nextButton = document.getElementById("slArrowNext");
- 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slidesContainer.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
- 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slidesContainer.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
